1db834a9403467c648cd85e443b16afe2d28d5d1
Meisoner <58383223+Meisoner@users.noreply.github.com>
2022-01-31 17:46:52 +0400

    spn

---
+++ spn.py
@@ -0,0 +1,16 @@
+import requests
+from dictutils import *
+
+
+def get_spn(coordinates):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": coordinates,
+        "format": "json",
+    }
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    json_response = response.json()
+    up = keydictsearch(json_response, 'uppercorner')[0].split()
+    down = keydictsearch(json_response, 'lowercorner')[0].split()
+    return str(float(up[0]) - float(down[0])) + ' ' + str(float(up[1]) - float(down[1]))
